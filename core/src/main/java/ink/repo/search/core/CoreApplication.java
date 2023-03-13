910df5f5ec871b42f5d4553db56e81f871aa363e
Bohui WU <bohuibhwu@gmail.com>
2023-03-13 15:44:22 +0800

    Add: Enable caching

--- core/src/main/java/ink/repo/search/core/CoreApplication.java
+++ core/src/main/java/ink/repo/search/core/CoreApplication.java
@@ -3,9 +3,11 @@ package ink.repo.search.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableCaching
 public class CoreApplication {
 
 	public static void main(String[] args) {
